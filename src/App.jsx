30d8fffcd78850667faaa571f77a6ab346a199ab
kaushal kumar <kaushalkumar50009@gmail.com>
2023-10-30 01:54:58 +0530

    crating custom hooks and higherOrder component

--- src/App.jsx
+++ src/App.jsx
@@ -16,14 +16,25 @@ import Footer from './components/footer/Footer'
 function App() {
 
   const dispatch = useDispatch()
-  const { url } = useSelector(state => state.home)
+  // const { url } = useSelector(state => state.home)
 
-  const apiTesting = () => {
-    fetchDataFromApi("/movie/popular").then((res) => dispatch(getApiConfiguration(res)))
+  const fetchApiConfig = () => {
+    fetchDataFromApi("/configuration").then((res) => {
+
+      const url = {
+        backdrop: res.images.secure_base_url + "original",
+        poster: res.images.secure_base_url + "original",
+        profile: res.images.secure_base_url + "original",
+      }
+
+
+      dispatch(getApiConfiguration(url))
+    }
+    )
   }
 
   useEffect(() => {
-    apiTesting();
+    fetchApiConfig();
   }, [])
 
   return (
--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -1,12 +1,24 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import "./style.scss"
 import { useNavigate } from "react-router-dom"
+import useFetch from '../../../hooks/usefetch'
+import { useSelector } from "react-redux"
+import Img from '../../../components/lazyLoadImage/Img'
+import ContentWrapper from '../../../components/contentWrapper/ContentWrapper'
+
 
 const HeroBanner = () => {
 
     const [background, setBackground] = useState("")
     const [query, setQuery] = useState("")
     const navigate = useNavigate()
+    const { url } = useSelector((state) => state.home)
+    const { data, loading } = useFetch("/movie/upcoming")
+
+    useEffect(() => {
+        const bg = url.backdrop + data?.results?.[Math.floor(Math.random() * 20)]?.backdrop_path
+        setBackground(bg)
+    }, [data])
 
     const searchQueryHandler = (e) => {
         if (e.key === "Enter" && query.length > 0) {
@@ -16,7 +28,16 @@ const HeroBanner = () => {
 
     return (
         <div className='heroBanner'>
-            <div className="wrapper">
+            {
+                !loading && <div className="backdrop-img">
+                    <Img src={background} />
+                </div>
+            }
+
+            <div className="opacity_layer"></div>
+
+            <ContentWrapper>
+
                 <div className="heroBannerContent">
                     <span className="title">Welcome</span>
                     <span className="subTitle">Discover movies</span>
@@ -26,7 +47,7 @@ const HeroBanner = () => {
                         <button>Search</button>
                     </div>
                 </div>
-            </div>
+            </ContentWrapper>
 
         </div>
     )
