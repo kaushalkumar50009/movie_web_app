cc532b9b2373210f74647fffa08dfeb1e3fe8400
kaushal kumar <kaushalkumar50009@gmail.com>
2023-10-30 00:40:03 +0530

    starting page and codes

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useEffect } from 'react'
+import './App.css'
+import { fetchDataFromApi } from './utils/api'
+import { useDispatch, useSelector } from "react-redux"
+import { getApiConfiguration } from './store/homeSlice'
+function App() {
+
+  const dispatch = useDispatch()
+  const { url } = useSelector(state => state.home)
+
+  const apiTesting = () => {
+    fetchDataFromApi("/movie/popular").then((res) => dispatch(getApiConfiguration(res)))
+  }
+
+
+  useEffect(() => {
+    apiTesting();
+  }, [])
+
+  return (
+    <>
+      <div className="App">{
+        url?.total_pages
+      }
+      </div>
+    </>
+  )
+}
+
+export default App
