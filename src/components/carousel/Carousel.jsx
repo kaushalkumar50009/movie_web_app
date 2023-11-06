c341ba708edc8365a20e09382367f4796db7e3ec
kaushal kumar <kaushalkumar50009@gmail.com>
2023-11-06 15:36:59 +0530

    created genres

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -12,6 +12,7 @@ import Img from "../lazyLoadImage/Img";
 import PosterFallback from "../../assets/no-poster.png";
 import "./style.scss";
 import CircleRating from "../circleRating/CircleRating";
+import Genres from "../genres/Genres";
 
 const Carousel = ({ data, loading }) => {
     const navigate = useNavigate();
@@ -51,6 +52,7 @@ const Carousel = ({ data, loading }) => {
                                             <div className="posterBlock">
                                                 <Img src={posterUrl} />
                                                 <CircleRating rating={item.vote_average.toFixed(1)} />
+                                                <Genres data={item.genre_ids.slice(0, 2)} />
                                             </div>
                                             <div className="textBlock">
                                                 <span className="title">
