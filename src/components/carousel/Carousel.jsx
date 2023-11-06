134b32e69737cf925beb793de0ef4afdb2e0284b
kaushal kumar <kaushalkumar50009@gmail.com>
2023-11-06 14:40:08 +0530

    added name and date

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -13,7 +13,6 @@ import PosterFallback from "../../assets/no-poster.png";
 import "./style.scss";
 
 const Carousel = ({ data, loading }) => {
-
     const navigate = useNavigate();
     const carouselContainer = useRef(null);
     const { url } = useSelector((state) => state.home)
@@ -28,20 +27,26 @@ const Carousel = ({ data, loading }) => {
             <ContentWrapper>
                 <BsFillArrowLeftCircleFill className="carouselLeftNav arrow" onClick={() => navigate("left")} />
 
-                <BsFillArrowRightCircleFill className="carouselRightNav arrow" onClick={() => navigate("right")} />
+                <BsFillArrowRightCircleFill className="carouselRighttNav arrow" onClick={() => navigate("right")} />
 
                 {
                     !loading ? (
                         <div className="carouselItems">
                             {
                                 data?.map((item) => {
-
                                     const posterUrl = item.poster_path ? url.poster + item.poster_path : PosterFallback
-
                                     return (
                                         <div className="carouselItem" key={item.id} >
                                             <div className="posterBlock">
                                                 <Img src={posterUrl} />
+                                            </div>
+                                            <div className="textBlock">
+                                                <span className="title">
+                                                    {item.title || item.name}
+                                                </span>
+                                                <span className="date">
+                                                    {dayjs(item.release_date).format("MMM D, YYYY")}
+                                                </span>
                                             </div>
                                         </div>
                                     )
