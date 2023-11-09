5c2c5f62fefddd95fecd7e6769d6bcd123d8cb06
kaushal kumar <kaushalkumar50009@gmail.com>
2023-11-09 14:32:14 +0530

    successfully created a details page

--- src/pages/details/detailsBanner/DetailsBanner.jsx
+++ src/pages/details/detailsBanner/DetailsBanner.jsx
@@ -21,7 +21,8 @@ const DetailsBanner = ({ video, crew }) => {
 
     const _genres = data?.genres?.map((g) => g.id)
 
-    // const director
+    const director = crew?.filter((f) => f.job === "Director")
+    const writer = crew?.filter((f) => f.job === "Screenplay" || f.job === "Writer")
 
     const toHoursAndMinutes = (totalMinutes) => {
         const hours = Math.floor(totalMinutes / 60);
@@ -123,6 +124,60 @@ const DetailsBanner = ({ video, crew }) => {
                                                     )
                                                 }
                                             </div>
+                                            {
+                                                director?.length > 0 && (
+                                                    <div className="info">
+                                                        <span className="text bold">
+                                                            Director: {""}
+                                                        </span>
+                                                        <span className="text">
+                                                            {
+                                                                director?.map((d, i) => (
+                                                                    <span key={i}>
+                                                                        {d.name}{director.length - 1 !== i && ", "}
+                                                                    </span>
+                                                                ))
+                                                            }
+                                                        </span>
+                                                    </div>
+                                                )
+                                            }
+                                            {
+                                                writer?.length > 0 && (
+                                                    <div className="info">
+                                                        <span className="text bold">
+                                                            Writer: {""}
+                                                        </span>
+                                                        <span className="text">
+                                                            {
+                                                                writer?.map((d, i) => (
+                                                                    <span key={i}>
+                                                                        {d.name}{writer.length - 1 !== i && ", "}
+                                                                    </span>
+                                                                ))
+                                                            }
+                                                        </span>
+                                                    </div>
+                                                )
+                                            }
+                                            {
+                                                data?.created_by?.length > 0 && (
+                                                    <div className="info">
+                                                        <span className="text bold">
+                                                            Creator: {""}
+                                                        </span>
+                                                        <span className="text">
+                                                            {
+                                                                data?.created_by?.map((d, i) => (
+                                                                    <span key={i}>
+                                                                        {d.name}{data?.created_by.length - 1 !== i && ", "}
+                                                                    </span>
+                                                                ))
+                                                            }
+                                                        </span>
+                                                    </div>
+                                                )
+                                            }
                                         </div>
                                     </div>
                                 </ContentWrapper>
